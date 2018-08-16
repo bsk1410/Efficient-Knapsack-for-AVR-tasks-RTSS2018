38e3b60ae95621659830747c2ac7e7c5406a7e2a
Aaron <aarontwillcock@gmail.com>
2018-08-16 14:36:27 -0400

    *.txt - Updated run data runAll.py - Added argparser

--- runAll.py
+++ runAll.py
@@ -2,10 +2,22 @@
 import NewMultiAVR  #Knapsack Algorithm
 import DRTMultiAVR  #DRT Algorithm
 import plotGraphs   #Graph plotting script
+import argparse     #To read command line arguments
 import sys          #Handle command-line arguments
 
+#Parse Command-Line Arguments
+parser = argparse.ArgumentParser(description='Run the Knapsack and DRT AVR Task Demand Calculations, graph results')
+parser.add_argument('-r','--runs', metavar='N', type=int,default='1',help ='Enter the number of times to run per Mode Count (positive integer)')
+args = parser.parse_args()
+
+#Validate Input
+if(args.runs < 0):
+    print("Negative run count. Runs must be 0 or greater")
+
+#Accept input
+runsPerMode = args.runs
+
 #Input Args
-runsPerMode = int(sys.argv[1])
 print("Executing ",runsPerMode," run(s) per Mode Count")
 
 
