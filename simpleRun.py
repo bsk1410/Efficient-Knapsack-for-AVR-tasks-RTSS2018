59a23fb5977908a450256c3e40218e365cbc0c97
Aaron <aarontwillcock@gmail.com>
2018-08-16 12:56:30 -0400

    simpleRun.py - Added command-line arg for runsPerMode

--- simpleRun.py
+++ simpleRun.py
@@ -2,13 +2,19 @@
 import NewMultiAVR
 import DRTMultiAVR
 import plotGraphs
+import sys
+
+#Input Args
+runsPerMode = sys.argv[1]
 
 #Run NewAlg for Mode count 6 through 15
-NewMultiAVR.NewMultiAVRgen()
+for r in runsPerMode:
+    NewMultiAVR.NewMultiAVRgen()
 
 #Run DRT Alg for Mode count 6 through 15
-for i in range(6,16):
-    DRTMultiAVR.DRTMultiAVRgen(i)
+for r in runsPerMode:
+    for i in range(6,16):
+        DRTMultiAVR.DRTMultiAVRgen(i)
 
 #Compare Graphs
 plotGraphs.multiAVRPlot()
