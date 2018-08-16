1ec47c37e4f47d96e9f06bda3ad5e9b305ba1beb
Aaron <aarontwillcock@gmail.com>
2018-08-16 13:19:35 -0400

    *.txt - Updated run data simpleRun.py - Added print statements / user feedback for run #

--- simpleRun.py
+++ simpleRun.py
@@ -6,13 +6,19 @@ import sys
 
 #Input Args
 runsPerMode = int(sys.argv[1])
+print("Executing ",runsPerMode," run(s) per Mode Count")
+
 
 #Run NewAlg for Mode count 6 through 15
+print("---Knapsack-Based Algorithm---")
 for r in range(0,runsPerMode):
+    print("\tExecuting Run ",r," of ", runsPerMode," for Mode Counts 6-15")
     NewMultiAVR.NewMultiAVRgen()
 
 #Run DRT Alg for Mode count 6 through 15
+print("---DRT Algorithm---")
 for r in range(0,runsPerMode):
+    print("\tExecuting Run ",r," of ", runsPerMode," for Mode Counts 6-15")
     for i in range(6,16):
         DRTMultiAVR.DRTMultiAVRgen(i)
 
